a8dc61a4995510d1d9d4a35a7aab01d928a50dde
echo <hello@kevinjamesneal.us>
2024-10-16 16:53:44 -0400

    fix
    v2

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const dotenv = require("dotenv");
 dotenv.config;
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+};
 
 module.exports = nextConfig;
